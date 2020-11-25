9d70bb443c23f2a34676aa539e708ae00961c3a0
nicholas-roether <nicholas.roether@t-online.de>
2020-11-25 16:39:22 +0100

    Add additional parsing

--- src/models/Subject.ts
+++ src/models/Subject.ts
@@ -1,14 +1,38 @@
 class Subject {
 	private static readonly subjectNames: {[key: string]: string} = {
 		"D": "Deutsch",
+		"De": "Deutsch",
 		"E": "Englisch",
 		"M": "Mathe",
+		"Geo": "Geografie",
+		"Phy": "Physik",
+		"Ku": "Kunst",
+		"Sp": "Sport",
+		"Sn": "Spanisch",
+		"Fr": "Französisch",
+		"Bio": "Biologie",
+		"Ch": "Chemie",
+		"Gebi": "Geschichte Bilingual",
+		"Sem": "Seminar"
 		// TODO add rest
 	}
 
+	private static readonly courseTypes: {[key: string]: string} = {
+		"g": "Grundkurs",
+		"L": "Leistungskurs"
+	}
+
+	private static get courseRegex() {
+		return new RegExp(`^(${Object.keys(this.subjectNames).join("|")})(${Object.keys(this.courseTypes).join("|")})?([0-9]+)?$`)
+	}
+
 	private static parseSubject(name: string) {
-		if(Object.keys(this.subjectNames).includes(name)) return this.subjectNames[name];
-		return name;
+		const res = this.courseRegex.exec(name);
+		if(!res) return name;
+		const subjectName = this.subjectNames[res[1]];
+		const courseTypeKey = res[2];
+		const courseNumber = res[3];
+		return `${subjectName}${courseTypeKey ? " " + this.courseTypes[courseTypeKey] : ""}${courseNumber ? " " + courseNumber : ""}`;
 	}
 
 	public readonly shortName: string;
