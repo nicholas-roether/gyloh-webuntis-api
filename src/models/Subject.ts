bc200f6f6cab333ffd5e6c25e417a7e348d43664
nicholas-roether <nicholas.roether@t-online.de>
2020-11-25 14:06:37 +0100

    Create Subject model

---
+++ src/models/Subject.ts
@@ -0,0 +1,21 @@
+class Subject {
+	private static readonly subjectNames: {[key: string]: string} = {
+		"D": "Deutsch",
+		"E": "Englisch",
+		"M": "Mathe",
+		// TODO add rest
+	}
+
+	private static parseSubject(name: string) {
+		if(Object.keys(this.subjectNames).includes(name)) return this.subjectNames[name];
+		return name;
+	}
+
+	public readonly shortName: string;
+	public readonly longName: string;
+
+	constructor(name: string) {
+		this.shortName = name;
+		this.longName = Subject.parseSubject(name);
+	}
+}
