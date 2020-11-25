78fc48d4ceaf78acb4c675af0a1b7164fc537f73
nicholas-roether <nicholas.roether@t-online.de>
2020-11-25 14:06:56 +0100

    Create Message model

---
+++ src/models/Message.ts
@@ -0,0 +1,9 @@
+class Message {
+	public readonly subject: string;
+	public readonly body: string;
+
+	constructor(subject: string, body: string) {
+		this.subject = subject;
+		this.body = body;
+	}
+}
